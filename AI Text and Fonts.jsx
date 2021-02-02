286bcd700312a697ad5f31b8aef66b3d5e1cf3bd
Nate Lovell <Natelovell99@gmail.com>
2021-02-02 01:03:43 -0700

    Create AI Text and Fonts.jsx

---
+++ AI Text and Fonts.jsx
@@ -0,0 +1,28 @@
+﻿// ai text and fonts
+var document = app.activeDocument;
+var newLayer = document.layers.add();
+newLayer.name = "Text Layer Test";
+var newTextFrame = newLayer.textFrames.add();
+$.writeln(newTextFrame.characters);
+$.writeln(newTextFrame.words);
+newTextFrame.contents = "Test Injection of Contents";
+//newTextFrame.anchor = [document.width*.5, document.height*.5];
+
+var fonts = app.textFonts;
+
+var desiredFont;
+for(var i = 0; i < fonts.length; i++) {
+    //$.writeln(fonts[i].name);
+    //$.writeln(fonts[i].style);
+    //$.writeln(fonts[i].family);
+    if(i == 40) {
+        desiredFont = fonts[i];
+        }
+    }
+
+var textRange = newTextFrame.textRange;
+var characterAttributes = textRange.characterAttributes;
+characterAttributes.textFont = desiredFont;
+characterAttributes.size = 48;
+
+newTextFrame.translate(document.width*.5-newTextFrame.width*.5, -document.height*.5);
